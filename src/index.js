b4ca011f81de8b938cfe99d3c5260a4737a702f3
mrfachri19 <fachryfachry1997@gmail.com>
2023-08-04 16:02:39 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './index.css';
 
@@ -24,8 +22,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-
-// serviceWorkerRegistration.register();
