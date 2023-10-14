045c81045942a080c130d97eb07965478706bfd9
mrfachri19 <fachryfachry1997@gmail.com>
2023-10-14 17:27:31 +0700

    update prioritas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-import("../App");
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import './index.css';
+
+// layouts
+
+import Admin from "./layouts/Admin";
+import Auth from "./layouts/Auth.js";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      {/* add routes with layouts */}
+      <Route path="/admin" component={Admin} />
+      <Route path="/auth" component={Auth} />
+      {/* add redirect for first page */}
+      <Redirect from="*" to="/admin" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
